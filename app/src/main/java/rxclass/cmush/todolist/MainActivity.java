d6e8ce645882b5c52548b4088c5a586950276dd7
MusH <collinsmucheru@gmail.com>
2019-08-08 04:33:02 +0300

    create a simple obervables, log activity

--- app/src/main/java/rxclass/cmush/todolist/MainActivity.java
+++ app/src/main/java/rxclass/cmush/todolist/MainActivity.java
@@ -3,12 +3,68 @@ package rxclass.cmush.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+import rxclass.cmush.todolist.models.Task;
+import rxclass.cmush.todolist.util.DataSource;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    //ui
+    private TextView text;
+
+    //vars
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+                        try {
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return task.isComplete();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: called.");
+            }
+
+            @Override
+            public void onNext(Task task) { // run on main thread
+                Log.d(TAG, "onNext: : " + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: : " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: : " + e);
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 }
